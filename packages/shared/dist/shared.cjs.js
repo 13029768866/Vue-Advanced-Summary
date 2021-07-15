c5d009bfd5b7817e4f7906103338baa399f0048e
woodensRangerWZJ <13029768866@163.com>
2021-07-15 10:12:42 +0800

    init

---
+++ packages/shared/dist/shared.cjs.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const isObject = (val) => typeof val === "object" && val !== null;
+const extend = Object.assign;
+
+exports.extend = extend;
+exports.isObject = isObject;
+//# sourceMappingURL=shared.cjs.js.map
---
+++ packages/shared/dist/shared.esm-bundler.js
@@ -0,0 +1,5 @@
+const isObject = (val) => typeof val === "object" && val !== null;
+const extend = Object.assign;
+
+export { extend, isObject };
+//# sourceMappingURL=shared.esm-bundler.js.map
