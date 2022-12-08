2b3e50e67137088af6a4f2a8bde40e60cfc563f5
13029768866@163.com <q3565576>
2022-12-08 15:50:50 +0800

    refactor(feat): reactive方法基础版

--- packages/reactivity/dist/reactivity.js
+++ packages/reactivity/dist/reactivity.js
@@ -3,6 +3,40 @@ var isObject = (value) => {
   return value !== null && typeof value === "object";
 };
 
-// packages/reactivity/src/index.ts
-console.log(isObject({ a: 1 }));
+// packages/reactivity/src/reactive.ts
+var ReactiveFlags = /* @__PURE__ */ ((ReactiveFlags2) => {
+  ReactiveFlags2["IS_REACTIVE"] = "__v_isReactive";
+  return ReactiveFlags2;
+})(ReactiveFlags || {});
+var proxyMap = /* @__PURE__ */ new WeakMap();
+function reactive(target) {
+  if (!isObject(target))
+    return target;
+  const existingProxy = proxyMap.get(target);
+  if (existingProxy)
+    return existingProxy;
+  if (target["__v_isReactive" /* IS_REACTIVE */])
+    return target;
+  const proxy = new Proxy(target, {
+    get(target2, key, receiver) {
+      if (key === "__v_isReactive" /* IS_REACTIVE */)
+        return true;
+      return Reflect.get(target2, key, receiver);
+    },
+    set(target2, key, value, receiver) {
+      return Reflect.set(target2, key, value, receiver);
+    }
+  });
+  proxyMap.set(target, proxy);
+  return proxy;
+}
+
+// packages/reactivity/src/effect.ts
+function effect() {
+}
+export {
+  ReactiveFlags,
+  effect,
+  reactive
+};
 //# sourceMappingURL=reactivity.js.map
